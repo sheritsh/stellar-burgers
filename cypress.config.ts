74822eeec0b228251e71dd67923a80f138d82ecc
Oleg Polovinko <sheritsh.s21@gmail.com>
2026-09-10 20:30:00 +0300

    chore: upgrade cypress to version 16

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  defaultBrowser: 'chrome',
   e2e: {
     baseUrl: 'http://localhost:4000',
     supportFile: 'cypress/support/e2e.ts'
